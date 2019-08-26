75d6a3cee95e022c674c778387917d9c54fe6313
HBurakKaradag <h.burakkaradag@gmail.com>
2019-08-26 14:27:55 +0300

    Errors and UIResponse Added

---
+++ BookingSystem.WebUI/Models/Response/Errors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.WebUI.Models.Response
+{
+    public class Errors
+    {
+        public string Key { get; set; }
+        public string[] ErrorMessages { get; set; }
+        public Exception[] Exceptions { get; set; }
+    }
+}
---
+++ BookingSystem.WebUI/Models/Response/UIResponse.cs
@@ -0,0 +1,17 @@
+﻿using BookingSystem.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.WebUI.Models.Response
+{
+    public class UIResponse
+    {
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        public OperationResultType ResultType { get; set; }
+    }
+}
