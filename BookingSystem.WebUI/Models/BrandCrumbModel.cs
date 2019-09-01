f52bc933ad25d0ad6d6205cdf0f3d1bcc98b5eef
HBurakKaradag <h.burakkaradag@gmail.com>
2019-09-01 16:15:48 +0300

    .

---
+++ BookingSystem.WebUI/Models/BrandCrumbModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.WebUI.Models
+{
+    public class BrandCrumbModel
+    {
+        public string ClassName { get; set; }
+
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+}
---
+++ BookingSystem.WebUI/Models/DateTimeModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using BookingSystem.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSystem.WebUI.Models
+{
+    public class DateTimeModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var cultureInfo = controllerContext.HttpContext.Request.Cookies["cultureInfo"];
+            var currentCulcure = "tr-TR";
+            if (cultureInfo != null)
+                currentCulcure = cultureInfo.Value;
+
+            var valueProvider = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (valueProvider == null)
+                return null;
+            if (valueProvider.AttemptedValue.IsNull())
+                return null;
+            //var parseResult = DateTime.TryParse(valueProvider.AttemptedValue, new CultureInfo(currentCulcure), DateTimeStyles.None, out dateTime);
+            //if (parseResult)
+            //    return dateTime;
+            DateTime dateTime;
+            if (DateTime.TryParse(valueProvider.AttemptedValue, new CultureInfo(currentCulcure), DateTimeStyles.None, out dateTime))
+                return dateTime;
+
+            bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Invalid Date");
+
+            return null;
+        }
+    }
+}
