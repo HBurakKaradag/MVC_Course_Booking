b445493f534ded245877f6f61aabc1d59c496bce
HBurakKaradag <h.burakkaradag@gmail.com>
2019-09-05 02:18:54 +0300

    CheckBoxListItem added

---
+++ BookingSystem.WebUI/Models/CheckBoxListTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.WebUI.Models
+{
+    public class CheckBoxListTemplate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
